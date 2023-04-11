ea990270d9c91158e16cd566fe601c98dd41f23d
TheViero <128616582+TheViero@users.noreply.github.com>
2023-04-11 16:25:19 +0200

    proxy

---
+++ Proxy/Proxy/Class1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    internal class Class1
+    {
+        public interface IDatabase
+        {
+            List<string> GetData();
+            void SaveData(string data);
+        }
+
+        // реальна база даних
+        public class Database : IDatabase
+        {
+            public List<string> GetData()
+            {
+                // отримуємо дані з бази даних
+                return new List<string> { "data1", "data2", "data3" };
+            }
+
+            public void SaveData(string data)
+            {
+                // зберігаємо дані у базу даних
+            }
+        }
+
+        // проксі для бази даних
+        public class DatabaseProxy : IDatabase
+        {
+            private Database _database;
+            private User _user;
+
+            public DatabaseProxy(User user)
+            {
+                _user = user;
+                _database = new Database();
+            }
+
+            public List<string> GetData()
+            {
+                if (_user.HasAccessToDatabase)
+                {
+                    return _database.GetData();
+                }
+                else
+                {
+                    throw new Exception("User does not have access to the database.");
+                }
+            }
+
+            public void SaveData(string data)
+            {
+                if (_user.HasAccessToDatabase)
+                {
+                    _database.SaveData(data);
+                }
+                else
+                {
+                    throw new Exception("User does not have access to the database.");
+                }
+            }
+        }
+
+        // користувач
+        public class User
+        {
+            public string Username { get; set; }
+            public string Password { get; set; }
+            public bool HasAccessToDatabase { get; set; }
+
+            public User(string username, string password)
+            {
+                Username = username;
+                Password = password;
+                // перевірка доступу користувача до бази даних
+                HasAccessToDatabase = CheckAccessToDatabase();
+            }
+
+            private bool CheckAccessToDatabase()
+            {
+                // перевірка доступу до бази даних
+                // повертає true, якщо доступ дозволений
+                // повертає false, якщо доступ заборонений
+                return true;
+            }
+        }
+    }
+}
---
+++ Proxy/Proxy/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Proxy.Class1;
+
+namespace Proxy
+{
+    public partial class Form1 : Form
+    {
+        private User _user;
+        private IDatabase _database;
+        private TextBox txtData;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // ініціалізація користувача та бази даних
+            _user = new User("username", "password");
+            _database = new DatabaseProxy(_user);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // отримання даних з бази даних та відображення їх у текстовому полі
+                List<string> data = _database.GetData();
+                txtData.Text = string.Join(", ", data);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // збереження даних у базу даних
+                _database.SaveData(txtData.Text);
+                MessageBox.Show("Data saved successfully.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
+
